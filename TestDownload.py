960feb888b2d8a814f63bebd949aa4085abc9890
Corban Cristian <crs.corban@gmail.com>
2021-01-05 12:29:25 +0200

    Add files via upload

---
+++ TestDownload.py
@@ -0,0 +1,64 @@
+from selenium import webdriver
+import os
+import sys
+import random
+import time
+
+def browserInstalled(browser): # Verific existenta webdriver in locatia executabilului python. Daca returneaza fals, ar trebui sa dau skip la test
+    for i in os.listdir("{}{}".format(sys.path[4],os.sep)):
+        if browser in i:
+            return True
+    return False
+
+
+def processOpened(browser): # Verific daca webdriver este in lista proceselor ce ruleaza
+    return browser in os.popen('wmic process get description').read()
+
+
+def browserLaunch(browser,*path):
+    if "firefox" in browser:
+        return webdriver.Firefox()
+
+    if "chrome" in browser:
+        return webdriver.Chrome()
+
+    if "edge" in browser:
+        return webdriver.Edge() # webdriver-ul noului edge e ciudat...
+
+
+if __name__=='__main__': # Testing space
+
+    def login(browser):
+        driver = browserLaunch(browser)
+        driver.implicitly_wait(10)
+        driver.get("https://figshare.com/")
+
+        driver.find_element_by_xpath('//a[contains(@href,"https://figshare.com/account/login")]').click()
+
+        driver.find_element_by_xpath('//input[contains(@autocomplete,"email")]').send_keys("bqk79730@yuoia.com")
+
+        driver.find_element_by_xpath('//input[contains(@autocomplete,"password")]').send_keys("Bqk79730@yuoia.com")
+
+        driver.find_element_by_xpath('//button[contains(@type,"submit")]').click()
+
+
+        #print(driver.find_element_by_xpath('//span[text()="Invalid credentials"]').is_displayed())
+        #driver.close()
+
+    #login("edge")
+    #print(os.path.isfile(os.path.realpath('../../test.txt')))
+    #print("fixture.browser.chrome".split(".")[2])
+    def download(tag):
+        driver=browserLaunch(tag.split(".")[2])
+        driver.implicitly_wait(10)
+        driver.get("https://figshare.com/")
+
+        random.choice(driver.find_elements_by_xpath('//div[contains(@role,"article")]')).click()
+
+        #driver.find_element_by_xpath('//span[text()="Download"]').click()
+
+        driver.execute_script("arguments[0].click();", driver.find_element_by_xpath('//span[text()="Download"]'))
+        time.sleep(10)
+        #driver.close()
+
+    download("fixture.browser.chrome")
